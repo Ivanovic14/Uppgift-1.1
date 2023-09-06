673ba0bd7d8f43a03b2bd8ab19d9721809109207
ival06001 <ival06001@E-R90YE0B5>
2023-09-06 12:32:56 +0200

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nästkomannde rads uppgift är att säga meningen "Du är nästan klar med kapitel 1
+            Console.WriteLine("Du är nästan klar med kapitel 1."); //Nästa rad ska säga "Du har lärt dig att skriva ut " och \
+            Console.WriteLine("Du har lärt dig att skriva ut tecken som """ och "\"")
+
+        }
+    }
+}
